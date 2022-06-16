ce358275479b5ef8ccdd72ba6db86d6cd53171b6
Barry Melton <barry.melton@gmail.com>
2022-06-16 09:22:47 -0400

    Easy things easy, hard things harder

--- src/templates/Header.tsx
+++ src/templates/Header.tsx
@@ -5,7 +5,7 @@ import Logo from "../components/logo";
 
 export const Header = () => {
   return (
-    <div className="bg-white dark:bg-slate-900">
+    <div className="bg-white dark:bg-slate-900 min-h-max">
       <nav className="flex items-center justify-between flex-wrap bg-white dark:bg-slate-900 p-6 max-w-6xl mx-auto">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
           <Logo />
@@ -25,12 +25,24 @@ export const Header = () => {
             <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-slate-900 dark:text-slate-100 hover:text-red dark:hover:text-slate-300">
               Blog
             </a>
-            <input type="search" placeholder="Search" className="mx-4 p-3 focus:float-left focus:absolute focus:left-2 focus:right-2 focus:top-4" />
+            <label className="relative text-gray-400 focus-within:text-gray-600 ">
+              <svg className="pointer-events-none w-8 h-8 absolute top-1/2 transform -translate-y-1/2 left-3 pl-3" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                width="30px" height="30px" viewBox="0 0 30 30">
+              <path d="M28,26.6l-8.265-8.264C21.15,16.61,22,14.405,22,12c0-5.523-4.478-10-10-10S2,6.477,2,12c0,5.523,4.478,10,10,10
+                c2.404,0,4.61-0.85,6.336-2.265L26.6,28L28,26.6z M12,20c-4.418,0-8-3.582-8-8c0-4.418,3.582-8,8-8s8,3.582,8,8
+                C20,16.418,16.418,20,12,20z"/>
+              </svg>
+              <input 
+                type="search" 
+                placeholder="Search" 
+                className="pl-9 mx-4 p-3 bg-gray-100 dark:bg-gray-300 focus:top-4 border-2 border-slate-300 rounded-sm" 
+              />
+            </label>
           </div>
           <div className="">
             <a href="#" 
               className="
-                inline-block text-sm px-4 py-2 leading-none border rounded text-black dark:text-white border-black dark:border-white 
+                inline-block text-sm px-4 py-2 leading-none border rounded text-black dark:text-white border-slate-900 dark:border-white  
                 hover:border-transparent hover:text-slate-900 hover:bg-white mt-4 lg:mt-0
             ">Download</a>
           </div>
