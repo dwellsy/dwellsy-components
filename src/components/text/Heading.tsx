92f448db535f4c2afd4fd442f69e90425b1d5280
Barry Melton <barry.melton@gmail.com>
2022-06-17 09:21:24 -0400

    sane max-width?

--- src/components/text/Heading.tsx
+++ src/components/text/Heading.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent } from "react";
 
 type HeadingProps = {
-  level?: 1 | 2 | 3 | 4 | 5 | 6,
+  level?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6",
   size?: "sm" | "md" | "lg" | "xl",
   children?: React.ReactNode;
 }
@@ -9,17 +9,13 @@ type HeadingProps = {
 
 export const Heading: FunctionComponent<HeadingProps> = ({ level, size, children }) => {
   const headingLevel = level === null ? 3 : level; 
-  const CustomHeading = `h${headingLevel}` as keyof JSX.IntrinsicElements;
+  const CustomHeading = `${headingLevel}` as keyof JSX.IntrinsicElements;
   const base_classes = "mt-1 font-semibold text-white sm:text-slate-900 dark:sm:text-white";
   const size_class = size === null ? "md:text-2xl text-2xl" : `md:text-${size} text-${size}`;
 
   return (
     <CustomHeading className={`${base_classes} ${size_class}`}>{ children }</CustomHeading>
   );
-}
-
-export const Test = () => {
-  return <Heading size=
 }
 
 export default Heading;
