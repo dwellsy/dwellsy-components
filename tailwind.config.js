ce23d71a4fed461eb8064230e1c4b33918639ce5
Barry Melton <barry.melton@gmail.com>
2022-06-20 09:34:27 -0400

    Adds font, removes comments.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Gotham", "sans-serif"],
+      },
       colors: {
         purple: "#7172e8",
         "purple-dark": "#5654bf",
@@ -11,24 +14,6 @@ module.exports = {
       },
     },
   },
-  /*
-  theme: {
-    colors: {
-      white: "#ffffff",
-      blue: "#1fb6ff",
-      purple: "#7172e8",
-      "purple-dark": "#5654bf",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      // 'gray-light': '#d3dce6',
-      "gray-light": "#eaeaea",
-    },
-  },
-  */
   plugins: [require("tailwindcss-dark-mode")()],
   variants: ["dark", "dark-hover", "dark-group-hover", "dark-even", "dark-odd"],
 };
