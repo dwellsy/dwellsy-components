9064020f019c7885e2cc77e018fd56c6476f5614
Barry Melton <barry.melton@gmail.com>
2022-06-20 18:27:44 -0400

    Social icons

---
+++ src/components/icons/Facebook.tsx
@@ -0,0 +1,12 @@
+export const FacebookIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.63 11.55" className="h-7 m-x-1 mx-3">
+      <title>icon-brand-fb</title>
+      <g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1" className="text-white">
+        <path className="fill-black dark:fill-white" d="M6.72,11.55V7.49H8.07l.26-1.68H6.72V4.72a.84.84,0,0,1,1-.91H8.4V2.38a9.11,9.11,0,0,0-1.3-.11A2.06,2.06,0,0,0,4.9,4.53V5.81H3.43V7.49H4.9v4.06a5.81,5.81,0,1,1,1.82,0Z"/>
+      </g></g>
+    </svg>
+  )
+}
+
+export default FacebookIcon;
---
+++ src/components/icons/LinkedIn.tsx
@@ -0,0 +1,12 @@
+export const LinkedInIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10.5 10.5" className="h-7 mx-3">
+      <title>icon-brand-li</title>
+      <g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1">
+        <path className="fill-black dark:fill-white" d="M10.5.76v9a.76.76,0,0,1-.75.76h-9A.75.75,0,0,1,0,9.74v-9A.75.75,0,0,1,.75,0h9A.76.76,0,0,1,10.5.76ZM3.3,2.4a.91.91,0,1,0-.9.9A.91.91,0,0,0,3.3,2.4ZM3.18,9V4H1.62V9H3.18ZM9,6.25c0-1.35-.3-2.39-1.87-2.39a1.63,1.63,0,0,0-1.47.81h0V4H4.15V9H5.71V6.52c0-.65.12-1.29.93-1.29s.81.75.81,1.33V9H9Z"/></g>
+      </g>
+    </svg>
+  )
+}
+
+export default LinkedInIcon;
