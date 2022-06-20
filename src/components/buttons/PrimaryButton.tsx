befd499309c5d1ab9f06644aedd90805a827d89b
Barry Melton <barry.melton@gmail.com>
2022-06-20 09:45:12 -0400

    Adds pink

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -10,7 +10,7 @@ export const PrimaryButton = (props: ButtonProps) => {
     <button 
       className="px-10 py-3 text-sm text-white font-semibold rounded-full 
         border border-purple bg-purple
-        hover:text-white hover:bg-purple-dark hover:border-purple-dark
+        hover:text-white hover:bg-purple-800 hover:border-purple-800
     ">{ props.label }</button>
   );
 }
